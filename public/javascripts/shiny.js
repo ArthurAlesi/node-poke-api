639440c8792c4018c29d32072d1680bc82b97811
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-10-08 18:24:33 -0300

    Update shiny.js

--- public/javascripts/shiny.js
+++ public/javascripts/shiny.js
@@ -1,15 +1,11 @@
-function trocarShiny(id){
+function trocarShiny(id) {
 
     var img = document.getElementById(id)
 
     if (img.src == `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`) {
-        img.src =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`
-    } else if(img.src == `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`) {
-        img.src =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-        // img.src =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`
+        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`
+    } else if (img.src == `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`) {
+        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     }
-    //  else {
-    //     console.log("ola mundo 3")
-    // }
 
 }
